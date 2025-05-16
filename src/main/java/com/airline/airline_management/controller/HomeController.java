8c1bb7ad51b279e33e7ffdc34f534c42dea7d47d
Ashwini Bisen <ashwinibisen26@gmail.com>
2025-05-16 16:38:00 +0530

    Funtional Improvements

--- src/main/java/com/airline/airline_management/controller/HomeController.java
+++ src/main/java/com/airline/airline_management/controller/HomeController.java
@@ -8,6 +8,15 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to Airline Management System!";
+        return "<h1>Welcome to the Airline Management System!</h1>" +
+                "<p>Your journey begins here. Please choose an option below:</p>" +
+                "<ul>" +
+                "<li><a href=\"#\">Book a Ticket</a></li>" +
+                "<li><a href=\"#\">Cancel a Ticket</a></li>" +
+                "<li><a href=\"#\">Check Available Flights</a></li>" +
+                "<li><a href=\"#\">Flight Status</a></li>" +
+                "<li><a href=\"#\">Customer Support</a></li>" +
+                "</ul>" +
+                "<p>More features coming soon. Stay tuned!</p>";
     }
 }
